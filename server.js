5cd173947c9b4e754a1ee8c6cd5a866b72379c0c
Vince Masten <vmasten@me.com>
2021-03-24 21:33:01 -0700

    weather API is working

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+'use strict'
+
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const superagent = require('superagent');
 
 const dotenv = require('dotenv').config();
-
-const cors = require('cors');
-const weather = require('./data/weather.json');
+app.use(cors());
 
 class Forecast { 
   constructor(description, date) {
@@ -12,20 +15,34 @@ class Forecast {
   }
 }
 
-const weatherData = weather.data.map((obj) => {
-  return new Forecast(obj.weather.description, obj.valid_date);
-  }
-);
-
-const app = express();
-
-app.use(cors());
-
 const PORT = process.env.PORT || 3000;
-app.get('/weather', function (request, response) {
+
+function getWeather (request, response) {
   const lat = request.query.lat
   const lon = request.query.lon
-  response.send(weatherData);
-})
+  
+  superagent.get(`${process.env.WEATHER_URL}?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`)
+    .then(res => {
+      const currentWeather = res.body
+      const weatherData = currentWeather.data.map((obj) => {
+        return new Forecast(obj.weather.description, obj.valid_date);
+      });
+      response.send(weatherData);
+    })
+}
+
+function getMovies (request, response) {
+  const city = request.query.city
+
+  superagent.get(`${process.env.MOVIES_URL}?api_key=${process.env.MOVIE_API_KEY}&query=${city}`)
+  .then(res => {
+    const movieData = res.body.results;
+    console.log(movieData);
+  })
+}
+
+app.get('/weather', getWeather);
+app.get('/movies', getMovies)
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
